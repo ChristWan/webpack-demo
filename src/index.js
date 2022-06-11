cc207056c2b4118fc2be8f96c92e6fc5e5e80d1b
ChristWan <christyim21@gmail.com>
2022-06-11 13:15:03 +0800

    添加懒加载

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ div.innerHTML = `
 <img id="jpg1" src="${jpg}">
 `
 console.log(x)
+
+const button = document.createElement('button')
+button.innerHTML = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy.js')
+    promise.then((module) => {
+        module.default()
+    }, () => {
+        console.log('模块加载错误');
+    })
+}
+div.appendChild(button)
